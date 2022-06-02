7bb5e1e44e2475789156149fa64b3f7faadfa547
Symoon Zadid <symoon.zadid@brainstation-23.com>
2022-06-02 13:28:28 +0600

    Initial Commit

---
+++ Quizz AppsUITests/Quizz_AppsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Quizz_AppsUITestsLaunchTests.swift
+//  Quizz AppsUITests
+//
+//  Created by Symoon Zadid on 2/6/22.
+//
+
+import XCTest
+
+class Quizz_AppsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
